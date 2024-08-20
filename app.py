95c19bba0ef0773b633a4db19cec5b1eaef3af4d
Bruno Almeida <brunoadsba@hotmail.com>
2024-08-20 11:31:42 -0300

    testando para dar erro

--- app.py
+++ app.py
@@ -13,9 +13,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+# @app.route('/')  # Decorador removido
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!', 404  # Retornando um erro 404 em vez de 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
