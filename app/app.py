b8d840435bd462099c8837cf58be3a2779485d05
Bruno Almeida <brunoadsba@hotmail.com>
2024-08-21 19:18:22 -0300

    up50

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ metrics = PrometheusMetrics(app)
 
 @app.route('/')
 def hello_world():
-    return 'Vamos nessa Devopêros :)'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- tests/test_app.py
+++ tests/test_app.py
@@ -9,4 +9,4 @@ def client():
 def test_home_route(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b'Vamos nessa Devopêros :)'
+    assert response.data == b'Hello, World!'
