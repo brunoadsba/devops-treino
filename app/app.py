dd866abe9b7ed4254b964f8550b27a962e5db804
Bruno Almeida <brunoadsba@hotmail.com>
2024-08-21 19:15:42 -0300

    up48

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ metrics = PrometheusMetrics(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Vamos nessa Devopêros :)'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
