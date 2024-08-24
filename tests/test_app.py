1476dd8c0245804fd933c595a113e6b5b7c29acb
Bruno Almeida <brunoadsba@hotmail.com>
2024-08-24 17:02:47 -0300

    Testando app 52

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,18 @@
 import pytest
-from app.app import app 
+from app.app import app
 
 @pytest.fixture
 def client():
+    app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
-def test_home_route(client):
+def test_home_page(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b'Hello, World!'
+    assert b'Bem-vindo ao Mundo DevOps' in response.data
+
+def test_about_page(client):
+    response = client.get('/about')
+    assert response.status_code == 200
+    assert b'Sobre Bruno' in response.data
