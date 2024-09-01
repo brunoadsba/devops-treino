f12ac227c03812a728064dea3241310124b01f32
Bruno Almeida <brunoadsba@hotmail.com>
2024-09-01 16:55:19 -0300

    up

--- app/app.py
+++ app/app.py
@@ -12,5 +12,4 @@ def home():
 def about():
     return render_template('about.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+# Remove o app.run() quando for usar Gunicorn.
